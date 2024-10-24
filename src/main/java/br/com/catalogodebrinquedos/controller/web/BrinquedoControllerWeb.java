21d5aaa6be2fe0bb7dc9900f18677c44c5fd0fec
Adenilson <adeni@192.168.5.147>
2024-10-24 12:36:32 -0300

    Atualizações recentes 🚀
    - Adicionadas novas páginas `brinquedo.html` e `novobrinquedo.html` para
    visualização detalhada dos brinquedos e para adicionar novos brinquedos
    ao catálogo.
    - Atualizada a página `brinquedosporcategoria.html` para uma melhor
    organização dos brinquedos de acordo com suas categorias.
    - Melhorias gerais no estilo (`style.css`) para suportar o novo layout
    das páginas e oferecer uma experiência de usuário aprimorada.
    - Adicionadas novas imagens e atualizadas as imagens de categoria para
    refletir melhor os produtos disponíveis.
    
    Essas mudanças foram implementadas para melhorar a experiência do
    usuário ao navegar pelo catálogo e adicionar novos brinquedos! 👍

--- src/main/java/br/com/catalogodebrinquedos/controller/web/BrinquedoControllerWeb.java
+++ src/main/java/br/com/catalogodebrinquedos/controller/web/BrinquedoControllerWeb.java
@@ -1,12 +1,15 @@
 package br.com.catalogodebrinquedos.controller.web;
 
 import br.com.catalogodebrinquedos.model.BrinquedoModel;
+import br.com.catalogodebrinquedos.model.CategoriaModel;
 import br.com.catalogodebrinquedos.model.repository.BrinquedoRepository;
+import br.com.catalogodebrinquedos.model.repository.CategoriaRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -18,6 +21,9 @@ public class BrinquedoControllerWeb {
 
     @Autowired
     private BrinquedoRepository brinquedoRepository;
+    
+    @Autowired
+    private CategoriaRepository categoriaRepository;
 
     @GetMapping("/home")
     public String home(Model model) {
@@ -33,5 +39,21 @@ public class BrinquedoControllerWeb {
         List<BrinquedoModel> brinquedos = brinquedoRepository.findAll();
         model.addAttribute("brinquedos", brinquedos);
         return "administracao";
+    }
+    
+    @GetMapping("/catalogodebrinquedos/brinquedo/{id}")
+    public String exibirDetalhesDoBrinquedo(@PathVariable Long id, Model model) {
+        BrinquedoModel brinquedo = brinquedoRepository.findById(id)
+            .orElseThrow(() -> new RuntimeException("Brinquedo não encontrado"));
+        model.addAttribute("brinquedo", brinquedo);
+        return "brinquedo";
+    }
+
+    @GetMapping("/administracao/novobrinquedo")
+    public String exibirFormularioNovoBrinquedo(Model model) {
+        List<CategoriaModel> categorias = categoriaRepository.findAll();
+        model.addAttribute("categorias", categorias);
+        model.addAttribute("brinquedo", new BrinquedoModel());
+        return "novobrinquedo";
     }
 }
